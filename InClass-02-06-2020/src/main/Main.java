06d1b5a2bde48500c27a2f7a4670fd8397572966
broylend <broylend@MCD210-PC-02.uccc.uc.edu>
2020-02-06 11:49:04 -0500

    Added some banking objects

--- InClass-02-06-2020/src/main/Main.java
+++ InClass-02-06-2020/src/main/Main.java
@@ -1,9 +1,20 @@
 package main;
 
+import bank.Bank;
+import bank.BrickAndMortar;
+import bank.Cafe;
+import bank.Online;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Bank bankPNC = new Bank();
+		BrickAndMortar bulidingPNC = new BrickAndMortar();
+		Online onlinePNC= new Online();
+		Cafe cafePNC = new Cafe();
+		
 
 	}
 
